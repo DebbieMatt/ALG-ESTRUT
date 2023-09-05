fdedda98dcf90ecaf7bc99a35635a6aaf8b38a28
Debbie Matt <112919058+DebbieMatt@users.noreply.github.com>
2023-09-05 16:48:34 -0300

    Create ATV 04.c

---
+++ AULA 12/ATV 04.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+char verificador(char resp){ // função 
+    
+switch (resp){
+    case 'a' ... 'z' :
+    return 1;
+    break;
+    
+    default:
+    return 0;
+    break;
+}
+}
+int main (){
+
+// Escreva uma função que após receber um caractere como entrada, retorne 1 se o caractere recebido for uma letra do alfabeto e 0 caso contrário
+
+char resp, resultado;
+
+printf("Escreva um caractere:\n");
+scanf("%c", &resp);
+
+resultado = verificador (resp);
+
+printf (" %d", resultado);
+}
