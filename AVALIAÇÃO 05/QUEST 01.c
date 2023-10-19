e1b3c032c840b7074117d1a2de8e44e4b780fbe1
Debbie Matt <112919058+DebbieMatt@users.noreply.github.com>
2023-10-18 22:01:40 -0300

    corrigir DO WHILE

---
+++ AVALIAÇÃO 05/QUEST 01.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main(){
+
+    int matriz[4][5], contador, intervalo;
+
+    // gerando a matriz
+
+    for (int linha = 0; linha < 4; linha ++){
+
+        for (int coluna = 0; coluna < 5; coluna++){
+
+            printf("Posição [%d][%d]: ", linha, coluna);
+            scanf("%d", &matriz[linha][coluna]);
+        }
+    }
+
+    // verificando se o numero digitado pertence ao intervalo
+
+    do { // corrigir do while
+
+        printf("\ndigite um numero entre 2 e 9: ");
+        scanf("%d", &intervalo);
+
+        if (( intervalo > 2) && (intervalo < 9)){
+
+        } 
+        else {
+        printf("digite novamente");
+        }
+
+    }while (( intervalo > 2) || (intervalo < 9));
+
+    printf("\nminha matriz:\n");
+
+    for (int linha = 0; linha < 4; linha ++){
+
+        for (int coluna = 0; coluna < 5; coluna++){
+
+            printf("\t%d\t", matriz[linha][coluna]);
+
+            // verificando se é multiplo do intervalo
+
+            if (matriz[linha][coluna]%intervalo == 0){
+
+                contador++;
+            }
+        }
+
+        printf("\n");
+    }
+
+    // imprimindo os multiplos
+
+    printf("\na matriz possui %d multimplos de %d.", contador, intervalo);
+
+}
